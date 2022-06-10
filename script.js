31c44f4dcb3c960072f4e0c78caff89cf1a51ccc
Diogo Rennam <diogorenan006@gmail.com>
2022-06-10 11:04:42 -0300

    adicionando js

---
+++ script.js
@@ -0,0 +1,42 @@
+const elementResposta = document.querySelector("#resposta")
+const elementInput = document.querySelector("#inputPergunta")
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
+
+
+//clicar em fazer pergunta 
+function fazerPergunta() {
+  if (elementInput.value == "") {
+    alert("Digite sua pergunta!")
+    return
+  }
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+  //gerar numero aleatorio
+const totalRespostas = respostas.length
+const numeroaleatorio = Math.floor(Math.random() * totalRespostas)
+console.log(respostas[numeroaleatorio])
+elementResposta.innerHTML = pergunta + respostas[numeroaleatorio]
+
+// sumir com a resposta depois de 3 segundos 
+setTimeout(function(){
+  elementResposta.style.opacity = 0 ;}, 3000)
+}
